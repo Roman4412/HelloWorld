1c272a772a30da817905df32786dcf7ae6681bdc
admin <kemrada62@gmail.com>
2022-12-03 05:41:33 +0300

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!1");
+        // 1
         var dog = 8;
         var cat = 3.6;
         var paper = 763789;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        // 2
         dog += 4;
         cat += 4 ;
         paper +=4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        // 3
+        dog -= 3.5;
+        cat -= 1.6;
+        paper -= 7639;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
